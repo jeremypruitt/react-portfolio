67b6bbad4185cfcb5c87be430e99fa8c7ccec947
Jeremy Pruitt <jeremypruitt@me.com>
2022-08-09 01:25:18 -0700

    Move to a component-based layout/design

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,17 @@
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { Link } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+    </div>
+  )
+}
+
+export default Sidebar
