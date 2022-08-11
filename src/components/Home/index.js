7521fdad9f26e893d1f1e0df7ba25e2f3cd1e644
Jeremy Pruitt <jeremypruitt@me.com>
2022-08-10 18:07:18 -0700

    Add new Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['e','r','e','m','y',' ','P','r','u','i','t','t']
+  const jobArray = ['s','o','f','t','w','a','r','e',' ','e','n','g','i','n','e','e','r']
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, []);
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={11}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={17}
+          />
+        </h1>
+        <h2>Infra & Platform Engineer / Architect / Manager / Leader</h2>
+        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home
