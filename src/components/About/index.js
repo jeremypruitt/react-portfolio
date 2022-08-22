f3a0b0a564a1039460b11e075c75c3e693f4e58c
Jeremy Pruitt <jeremypruitt@me.com>
2022-08-22 05:13:56 -0700

    Add animated rotating cube to About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,6 +2,15 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faAws,
+  faDocker,
+  faReact,
+  faHtml5,
+  faPython,
+  faGitAlt
+} from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -35,6 +44,28 @@ const About = () => {
             currently seeking a role where I can leverage my experience to
             effect meaningful change for an important cause.
           </p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAws} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faDocker} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faReact} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faHtml5} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faPython} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
